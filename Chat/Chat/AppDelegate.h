d35203cbbba7edd701b475ab7481cf34dc73d654
Semler <semler@SemlernoMacBook-Pro.local>
2017-06-25 14:10:03 +0900

    first

---
+++ Chat/Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by Semler on 2017/06/25.
+//  Copyright © 2017年 semler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chat/Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat
+//
+//  Created by Semler on 2017/06/25.
+//  Copyright © 2017年 semler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
